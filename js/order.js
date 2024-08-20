4ea66d2118cd56e8bfdc4f05c7f71fb797c9ef10
ChiaEnYang <grace86716@gmail.com>
2024-08-20 16:59:00 +0800

    js調整按鈕功能
    未完成

---
+++ js/order.js
@@ -0,0 +1,22 @@
+const minusBtns = document.querySelectorAll('.minus-btn');
+const addBtns = document.querySelectorAll('.add-btn');
+const orderNum = document.querySelectorAll('.order-number');
+
+
+// console.log(minusBtns);
+console.log(orderNum);
+console.log(orderNum[1]);
+console.log(orderNum[1].value);
+
+
+
+minusBtns.forEach((btn,index) => {
+    btn.addEventListener('click', ()=>{
+        
+        
+        
+    }
+    
+    )
+    // console.log(orderNum.value);
+});
